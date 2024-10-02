68693d9a12a53e8f499ee6009c38bc3ac8a1d10f
Kajkitsu1 <bartekjestanonimowy615@gmail.com>
2024-10-02 19:43:48 +0200

    Dodane zdjecia do animacji postaci
    Zdjęcia dodane. Kod nie działa. Wszystko się sypie. Walić Godota

--- movement.gd
+++ movement.gd
@@ -3,25 +3,26 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
+#ready var animation = $AnimationPlayer
+
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	
 	var b := Input.get_axis("ui_up", "ui_down")
 	if b:
 		velocity.y = b * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
