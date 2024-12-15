78b0d89c0cdc62e561c6b191c514e1ea4ea1779f
rzymianinCwr <pawel.pawlikowski@woli.edu.pl>
2024-12-15 16:24:23 +0100

    dodalem drugą armatke ale pocisk i tak nie leci poprawnie

--- cannon.gd
+++ cannon.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
-var bullet_scene = preload("res://bullet.tscn")  # Wczytanie sceny pocisku
-var fire_rate = 2.0  # Czas pomiędzy strzałami (w sekundach)
+var bullet_scene = preload("res://bullet.tscn")  # Załaduj scenę pocisku
+var bullet_speed = 500  # Prędkość pocisku
 
 func _ready():
-	# Konfiguracja Timera dla strzałów
-	$Timer.wait_time = fire_rate  # Ustaw czas między strzałami
-	$Timer.start()  # Uruchom Timer
+	# Sprawdź, czy węzeł Timer istnieje i ustaw go na strzały co 2 sekundy
+	if $Timer != null:
+		$Timer.wait_time = 2.0  # Czas pomiędzy strzałami
+		$Timer.start()  # Rozpocznij timer
+		$Timer.timeout.connect(_on_Timer_timeout)  # Połącz sygnał timeout z funkcją
+	else:
+		print("Błąd: Timer nie istnieje!")
 
 func _on_Timer_timeout():
-	# Funkcja wywoływana co zakończenie Timera - strzelanie pociskiem
-	var bullet = bullet_scene.instantiate()  # Stwórz instancję pocisku
-	bullet.position = global_position  # Ustaw początkową pozycję pocisku (miejsce armaty)
-	bullet.rotation = rotation  # Ustaw pocisk zgodnie z obrotem armaty
-	get_parent().add_child(bullet)  # Dodaj pocisk do sceny głównej
+	# Utwórz pocisk
+	var bullet = bullet_scene.instantiate()  # Utwórz nową instancję pocisku
+	bullet.position = $Muzzle.position  # Ustaw pozycję pocisku na pozycję węzła Muzzle
+	bullet.rotation = rotation  # Ustaw rotację pocisku na rotację armaty
+	bullet.speed = bullet_speed  # Ustaw prędkość pocisku
+	get_parent().add_child(bullet)  # Dodaj pocisk do głównej sceny
+	print("Pocisk utworzony w pozycji: ", bullet.position)  # Debugowanie
