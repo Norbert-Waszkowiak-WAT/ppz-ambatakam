bda720006760c7edd9ba884822ef6d3a1c6febbf
Kajkitsu1 <bartekjestanonimowy615@gmail.com>
2024-12-15 20:37:00 +0100

    Dodanie funkcji wyjścia z racer
    Dodanie funkcji wyjścia z racer. Niestety wynik się nie zapisuje. Musze to naprawić

--- racer.gd
+++ racer.gd
@@ -24,6 +24,19 @@ func _ready():
 	$Gameover.get_node("Button").pressed.connect(nowa_gra)
 	nowa_gra()
 
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		load_scene()
+
+func load_scene():
+	var scene_path = "res://main.tscn"
+	if ResourceLoader.exists(scene_path):
+		get_tree().change_scene_to_file("res://main.tscn")
+	else:
+		print("Failed to load scene 'main.tscn'. Scene does not exist.")
+
+
+
 func _process(delta: float):
 	if gra_on:
 		timer += delta
